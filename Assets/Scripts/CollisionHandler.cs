993331dd4945a54b4a010f9bb302c3f05f8045d7
Keefe Custodio <keefe.dev@gmail.com>
2022-11-18 18:42:54 -0800

    section 3 - respawn using SceneManager

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -19,8 +21,14 @@ public class CollisionHandler : MonoBehaviour
                 break;
 
             default:
-                Debug.Log("Obstacle");
+                ReloadLevel();
                 break;
         }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
